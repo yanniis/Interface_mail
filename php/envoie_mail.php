cafb9cee943c7f035ee709433c3e8d23a91bd879
Yannis Bouttier <yannisbouttier@MacBook-Pro-de-Yannis.local>
2023-05-12 14:06:38 +0200

    fichier php fait

---
+++ php/envoie_mail.php
@@ -0,0 +1,16 @@
+<?php
+if(isset($_POST['email'])) {
+    $to = $_POST['destinataire']; // Adresse e-mail du destinataire spécifiée dans le formulaire
+    $subject = "Nouveau message depuis le formulaire de contact";
+    $message = "Nom: " . $_POST['nom'] . "\n\n";
+    $message .= "Email: " . $_POST['email'] . "\n\n";
+    $message .= "Message: " . $_POST['message'] . "\n\n";
+    $headers = "From: " . $_POST['email'];
+
+    if(mail($to, $subject, $message, $headers)) {
+        echo "Votre e-mail a été envoyé avec succès.";
+    } else {
+        echo "Une erreur s'est produite lors de l'envoi de l'e-mail.";
+    }
+}
+?>
